482dcd7343f50a918e07ace2cd366a88d0a40772
sneha705 <705sneha@gmail.com>
2026-04-14 11:23:38 +0530

    feat: upgrade Next.js to version 16.2.3 and update related dependencies; refactor build scripts to use webpack

--- next.config.ts
+++ next.config.ts
@@ -6,22 +6,22 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
-  devIndicators: false,
   output: "standalone",
+
+  // 🔥 IMPORTANT → disable turbopack
+  // turbopack: false,
+
   env: {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-    // NEXT_PUBLIC_BASIC_AUTH: process.env.NEXT_PUBLIC_BASIC_AUTH,
-    // NEXT_PUBLIC_ASSET_URL: process.env.NEXT_PUBLIC_ASSET_URL
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
   typescript: {
     ignoreBuildErrors: true,
-    tsconfigPath: "./tsconfig.json",
   },
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'mdx'],
+
+  pageExtensions: ["ts", "tsx", "js", "jsx", "mdx"],
+
   webpack(config: any, { isServer }: any) {
     if (!isServer) {
       config.optimization.splitChunks = {
@@ -29,7 +29,7 @@ module.exports = withBundleAnalyzer({
         minSize: 20000,
         maxSize: 70000,
       };
-      // config.optimization.usedExports = true;
+
       config.plugins.push(new CompressionPlugin());
     }
 
